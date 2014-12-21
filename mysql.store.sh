33ecda57a7ff1081cf15d5f8e2fc1908b902bc0a
Dmitry Kushnikov <dkushnikov@gmail.com>
2014-12-21 17:12:59 +0300

    Fix mysql store script

--- mysql.store.sh
+++ mysql.store.sh
@@ -3,5 +3,6 @@ source config.sh
 
 read -s -p "Enter root password: " root_password
 
+mkdir -p $2
 mysqldump -h $db_mysql_host -p $db_mysql_port -u $db_mysql_username -p$root_password --single-transaction --no-create-db --no-data --routines --quick $1 > $2/$db_project_name.structure.sql
 mysqldump -h $db_mysql_host -p $db_mysql_port -u $db_mysql_username -p$root_password --single-transaction --no-create-db --no-create-info  --quick $1 > $2/$db_project_name.data.sql
