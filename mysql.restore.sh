3a4c402b430bb1a492349582066210bdbe1ca78b
Dmitry Kushnikov <dkushnikov@gmail.com>
2015-11-09 22:48:44 +0300

    Update mysql.restore.sh
    Fix syntax issue

--- mysql.restore.sh
+++ mysql.restore.sh
@@ -4,7 +4,7 @@ source config.sh
 
 read -s -p "Enter root password: " root_password
 
-mysqladmin -h $db_mysql_host -p $db_mysql_port -f -u $db_mysql_username -p$root_password drop $1
-mysqladmin -h $db_mysql_host -p $db_mysql_port -f -u $db_mysql_username -p$root_password create $1
-mysql      -h $db_mysql_host -p $db_mysql_port    -u $db_mysql_username -p$root_password $1 < $2/$db_project_name.structure.sql
-mysql      -h $db_mysql_host -p $db_mysql_port    -u $db_mysql_username -p$root_password $1 < $2/$db_project_name.data.sql
+mysqladmin --host=$db_mysql_host --port=$db_mysql_port --user=$db_mysql_username --password=$root_password --force drop $1
+mysqladmin --host=$db_mysql_host --port=$db_mysql_port --user=$db_mysql_username --password=$root_password --force create $1
+mysql      --host=$db_mysql_host --port=$db_mysql_port --user=$db_mysql_username --password=$root_password $1 < $2/$db_project_name.structure.sql
+mysql      --host=$db_mysql_host --port=$db_mysql_port --user=$db_mysql_username --password=$root_password $1 < $2/$db_project_name.data.sql
